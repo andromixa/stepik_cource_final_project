192ee4bdafb2ca6378d27268cc6e6499a655bb15
Mikheev <amikheev@neoflex.ru>
2023-12-25 14:27:02 +0500

    Implemented page change logic

--- pages/login_page.py
+++ pages/login_page.py
@@ -10,16 +10,12 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         url = self.browser.current_url
-        assert 'login' in url
+        assert 'login' in url, 'URL doesn\'t contain "login" substring'
 
     def should_be_login_form(self):
-        assert (
-            self.is_element_present(*LoginPageLocators.LOGIN_FORM),
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), \
             'Login form is not presented on the page'
-        )
 
     def should_be_register_form(self):
-        assert (
-            self.is_element_present(*LoginPageLocators.REGISTER_FORM),
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), \
             'Registration form is not presented on the page'
-        )
--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -7,5 +8,6 @@ def test_guest_can_go_to_login_page(browser):
     main_page = MainPage(browser, link)
     main_page.open()
     main_page.go_to_login_page()
-    main_page.should_be_login_link()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
