b69587a3a248b81f88b8171f5af1f0c7723f6673
Mikheev <amikheev@neoflex.ru>
2023-12-26 17:48:56 +0500

    Moved login-link functionality to BasePage. Added tests for ProductPage

--- test_product_page.py
+++ test_product_page.py
@@ -7,12 +7,12 @@ promo_links = [f'{link}?promo=offer{x}' for x in range(10)]
 promo_links[7] = pytest.param(promo_links[7], marks=pytest.mark.xfail)
 
 
-# @pytest.mark.parametrize('promo_link', promo_links)
-# def test_guest_can_add_product_to_basket(browser, promo_link):
-#     product_page = ProductPage(browser, promo_link)
-#     product_page.open()
-#     product_page.add_to_cart()
-#     product_page.should_be_added()
+@pytest.mark.parametrize('promo_link', promo_links)
+def test_guest_can_add_product_to_basket(browser, promo_link):
+    product_page = ProductPage(browser, promo_link)
+    product_page.open()
+    product_page.add_to_cart()
+    product_page.should_be_added()
 
 
 @pytest.mark.xfail(reason="under development")
@@ -35,3 +35,18 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     product_page.open()
     product_page.add_to_cart()
     product_page.should_be_disappeared_success_message(4)
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
